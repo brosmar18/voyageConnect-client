b19c98ef9f2cfded153cf5db404bf6ffdfd45e47
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-04 17:27:11 -0500

    Initial Commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import RootLayout from '@/app/layout';
+
+const home = () => {
+  return (
+    <RootLayout>
+        <div>
+            <h1>Home</h1>
+        </div>
+    </RootLayout>
+  )
+}
+
+export default home;
