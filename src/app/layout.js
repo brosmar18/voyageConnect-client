1db05080d0d329c59daf724b3bca0f785d6f0758
Bryan Garduno Gonzalez <bryangonzalez1896@icloud.com>
2023-09-04 18:08:26 -0500

    imports added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,20 @@
 import './globals.css';
 import { NavBar } from '../components';
-
+import authReducer from '../state';
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
 
 
 export default function RootLayout({ children }) {
